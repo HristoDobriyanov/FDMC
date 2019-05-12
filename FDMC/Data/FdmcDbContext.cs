8b304d5344f30fc48e8259d5aae3227cb8aa0312
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-05-12 15:07:24 +0300

    Add project files.

---
+++ FDMC/Data/FdmcDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FDMC.Data
+{
+    public class FdmcDbContext : IdentityDbContext
+    {
+        public FdmcDbContext(DbContextOptions<FdmcDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
