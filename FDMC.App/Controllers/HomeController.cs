1b8f06c9eeac64c8f355c845f6d955d607bacc44
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-05-29 14:41:34 +0300

    Updates

---
+++ FDMC.App/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FDMC.App.Models;
+using FDMC.Data;
+
+namespace FDMC.App.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController(FDMCAppDbContext context)
+        {
+            this.Context = context;
+        }
+
+        public FDMCAppDbContext Context { get; private set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FDMC.Data/FDMCAppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FDMC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FDMC.Data
+{
+    public class FDMCAppDbContext : DbContext 
+    {
+        public DbSet<Cat> Cats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-R4OGD90\SQLEXPRESS;
+                                                            Database=FDMC;
+                                                            Integrated Security=True");
+
+                
+            }
+        }
+    }
+}
